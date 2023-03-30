4fa246246cda40811ef7673fcbfbdafaa2292703
Nicolas Lima <nicolaslima1500@gmail.com>
2023-03-30 16:25:17 -0300

    Requisito 07 finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Header />
+        <SolarSystem />
+        <Missions />
       </div>
     );
   }
---
+++ src/components/Missions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">Missions</div>
+    );
+  }
+}
+
+export default Missions;
