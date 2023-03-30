0efc9298b42cc416bb8a04d6a1e0a4aef03082a6
Nicolas Lima <nicolaslima1500@gmail.com>
2023-03-30 16:11:15 -0300

    Requisitos 05 e 06 finalizados

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const planet = planets
+      .map((item) => (
+        <PlanetCard
+          key={ item.name }
+          planetName={ item.name }
+          planetImage={ item.image }
+        />
+      ));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planet }
       </div>
     );
   }
